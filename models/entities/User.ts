69e6d8d9a09c9d77b6433813d2d92cf06d15afc0
claudios883 <claudioalejandro4@gmail.com>
2022-10-22 15:11:15 -0500

    first commit

---
+++ models/entities/User.ts
@@ -0,0 +1,31 @@
+import {Entity, Column, PrimaryGeneratedColumn} from 'typeorm';
+
+@Entity({name: 'user'})
+export default class User {
+
+    @PrimaryGeneratedColumn({type:'int', unsigned: true})
+    public id: number;
+
+    @Column({type:'varchar', length:32, nullable: false, unique: true})
+    public username: string;
+    
+    @Column({type:'varchar', length:32, nullable: false})
+    public password: string;
+
+    @Column({type:'varchar', length:64, nullable: false, unique: true})
+    public mail: string;
+
+    @Column({type:'datetime', nullable: false})
+    public creationDateTime: Date;
+
+    @Column({type:'datetime', nullable:false,})
+    public updateDateTime: Date;
+
+    public constructor(_username: string, _password: string, _mail: string){
+        this.username = _username;
+        this.password = _password;
+        this.mail = _mail;
+        this.creationDateTime = new Date();
+        this.updateDateTime = new Date();
+    }
+}
